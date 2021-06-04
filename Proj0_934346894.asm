84ab573c3a120bd145ffe84a708e602501e693a9
Ethan Rietz <ewrietz@gmail.com>
2021-06-03 21:20:12 -0500

    Initial commit

---
+++ Proj0_934346894.asm
@@ -0,0 +1,32 @@
+TITLE <FIXME>     (FIXME.asm)
+
+; Author                : Ethan Rietz
+; Last Modified         : FIXME
+; OSU email address     : rietze@oregonstate.edu
+; Course number/section : CS271 Section 400
+; Project Number        : FIXME
+; Due Date              : FIXME
+; Description           : \
+;   FIXME
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+    Invoke ExitProcess,0    ; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
